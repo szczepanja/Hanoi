d9a6894082f1176247813a6c0c2625213549dcab
a_szczepanik <a.szczepanik@adform.com>
2022-03-15 12:35:05 +0100

    add initial state of a game and handle exception

--- Hanoi.scala
+++ Hanoi.scala
@@ -6,23 +6,44 @@ object Hanoi extends App {
   val disks = Array(mutable.Stack[Int](0, 1, 2), mutable.Stack[Int](), mutable.Stack[Int]())
 
   def moveDisk(from: Int, to: Int): Unit = {
-    if (disks(to).isEmpty || disks(from).head < disks(to).head) {
-      disks(to).push(disks(from).head)
-      disks(from) = disks(from).tail
-    } else {
-      println("Wrong move")
+    try {
+      if (disks(to).isEmpty || disks(from).head < disks(to).head) {
+        disks(to).push(disks(from).head)
+        disks(from) = disks(from).tail
+      }
+    } catch {
+      case i: ArrayIndexOutOfBoundsException => {
+        println("Wrong operator")
+      }
     }
   }
 
-  def play(): Unit = {
-    val x = readLine("Apply x: ").toInt
-    val y = readLine("Apply y: ").toInt
+  def initialState(): Unit = {
+    println("You are playing Tower of Hanoi. Move elements to another Stack(from 0 to 2) starting from 0")
+    renderStacks()
+  }
 
-    moveDisk(x, y)
+  def isGameFinished(): Unit = {
+    if (disks(0).isEmpty && disks(1).isEmpty) {
+      println("Congrats you won")
+      exit()
+    }
+  }
+
+  def renderStacks(): Unit = {
     for (d <- disks) println(d)
   }
 
-  println("You are playing Tower of Hanoi. Move elements to another Stack(from 0 to 2) starting from 0")
+  def play(): Unit = {
+    val x = readLine("Apply element you want to move: ").toInt
+    val y = readLine("Choose number of Stack where you want to move element: ").toInt
+
+    moveDisk(x, y)
+    renderStacks()
+    isGameFinished()
+  }
+
+  initialState()
   while (true) {
     play()
   }
